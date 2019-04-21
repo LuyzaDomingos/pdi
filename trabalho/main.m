649515b2e96dba0d8b7bf81e65ce43e44035f572
Luyza Domingos <luyzadomingos@cc.ci.ufpb.br>
2019-04-20 23:10:29 -0300

    questão 1 - adicionada

--- trabalho/main.m
+++ trabalho/main.m
@@ -1,33 +1,6 @@
 
+
 [audio,fs] = audioread('audio.wav'); %audio guarda os valores contidos no audio; fs guarda a frequencia
-
-function y = ourDCT(signal)
-  realx = isreal(signal);
-  transpose = (rows (signal) == 1);
-
-  if transpose, signal = signal (:); endif
-  [nr, nc] = size (signal);
-  
-  n = nr;
-
-  if n == 1
-    w = 1/2;
-  else
-    w = [ sqrt(1/4/n); sqrt(1/2/n)*exp((-1i*pi/2/n)*[1:n-1]') ] * ones (1, nc);
-  endif
-  
-  if ( realx && rem (n, 2) == 0 )
-    y = fft ([ signal(1:2:n,:) ; signal(n:-2:1,:) ]);
-    y = 2 * real( w .* y );
-  else
-    y = fft ([ signal ; flipud(x) ]);
-    y = w .* y (1:n, :);
-    if (realx) y = real (y); endif
-  endif
-  
-  if transpose, y = y.'; endif
-  
-endfunction
 
 while true
   disp("\n\n");
@@ -42,12 +15,9 @@ while true
     case 0
       break;
     case 1
-      y = ourDCT(audio);
-      disp(y);
-      disp("y");
-      %q = dct(audio);
-      %disp(q);
-      %disp("q");
+      questao1(audio);
+      audiowrite("audionew.wav",questao1_1(audio),fs);
+
 %    case 2
 %           
 %    case 3
@@ -55,12 +25,3 @@ while true
   endswitch    
 
   endwhile
-  
-
-
-
-
-
-
-
-
